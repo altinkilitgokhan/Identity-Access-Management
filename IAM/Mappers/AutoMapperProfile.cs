e369ef35d8a4d3ce1520b1bd6f24fa3407a70a53
gokhan.altinkilit <gokhan.altinkilit@odeontechnology.com>
2022-10-30 21:06:47 +0300

    Middleware implementation

---
+++ IAM/Mappers/AutoMapperProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using IAM.Api.Models;
+using IAM.Application.Models;
+using IAM.Domain.Entities;
+
+namespace IAM.Api.Mappers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, AuthenticateResponseModel>();
+            CreateMap<RegisterRequestModel, User>();
+            CreateMap<AuthenticateRequestModel, AuthenticateApplicationRequestModel>();
+
+            CreateMap<UpdateRequestModel, User>()
+                .ForAllMembers(x => x.Condition(
+                    (src, dest, prop) => 
+                    {
+                        if (prop == null) return false;
+                        if (prop.GetType() == typeof(string) && string.IsNullOrEmpty((string)prop)) return false;
+                        
+                        return true;
+                    }
+                 ));
+        }
+    }
+}
